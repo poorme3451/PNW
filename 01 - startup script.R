582a2b135f45cb986be72a60562b0942dc17d070
Daniel P <95542129+poorme3451@users.noreply.github.com>
2022-10-30 12:56:09 -0700

    Clean directory & startup

---
+++ 01 - startup script.R
@@ -0,0 +1,16 @@
+###############################################
+#
+#   Startup script
+#
+###############################################
+library(tidyverse)
+library(dplyr)
+library(dtplyr)
+library(janitor)
+library(ggplot2)
+
+setwd("~/Documents/Rprojects/PNW")
+
+
+###############################################
+#   End of File
